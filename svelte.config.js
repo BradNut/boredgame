8c0778f1fa5b00150c1a3ddf5ae14749aef751e5
Bradley Shellnut <bradleyshellnut@protonmail.com>
2022-04-17 23:16:17 -0700

    Updating dependencies and fixing to work with new svelte kit.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,10 +10,13 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
+		vite: {
+			resolve: {
+				alias: {
+					$root: path.resolve('./src')
+				}
+			}
+		},
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
