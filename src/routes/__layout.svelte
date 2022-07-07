b39f8ef1debe4a7362567aba120d427d2bdfd0c4
Bradley Shellnut <bradleyshellnut@protonmail.com>
2022-07-07 16:45:44 -0700

    Adding transition component and adding to layout for page transitions.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,36 +1,32 @@
 <script lang="ts">
   import Header from '$lib/header/Header.svelte';
+  import Transition from '$lib/components/transition/index.svelte';
   // import 'carbon-components-svelte/css/all.css';
   import '$root/styles/styles.scss';
 </script>
 
-<div class="fade" style:animation-duration="250ms" style:animation-delay="250ms">
-  <Header />
-  <main>
-    <slot />
-  </main>
-  <footer>
-    <p>Built by <a target="__blank" href="https://bradleyshellnut.com">Bradley Shellnut</a></p>
-    <p>
-      <a href="https://www.flaticon.com/free-icons/board-game" title="board game icons">Board game icons created by Freepik - Flaticon</a>
-    </p>
-  </footer>
-</div>
+<Transition transition={{ type: 'fade', duration: 250 }}>
+  <div class="wrapper">
+    <Header />
+    <Transition transition={{ type: 'page' }}>
+      <main>
+        <slot />
+      </main>
+    </Transition>
+    <footer>
+      <p>Built by <a target="__blank" href="https://bradleyshellnut.com">Bradley Shellnut</a></p>
+      <p>
+        <a href="https://www.flaticon.com/free-icons/board-game" title="board game icons">Board game icons created by Freepik - Flaticon</a>
+      </p>
+    </footer>
+  </div>
+</Transition>
 
 <style>
-  .fade {
+  .wrapper {
     display: grid;
     grid-template-rows: auto 1fr auto;
     min-height: 100vh;
-    animation-name: fadeIn;
-  }
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
   }
 
   main {
