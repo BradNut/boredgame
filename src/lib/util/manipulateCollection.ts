d739be11afb0d5ec52480ba58f221db45f1e6327
Bradley Shellnut <bradleyshellnut@protonmail.com>
2022-07-28 17:34:34 -0700

    Remove name from Toast.

--- src/lib/util/manipulateCollection.ts
+++ src/lib/util/manipulateCollection.ts
@@ -4,10 +4,10 @@ import { ToastType, type GameType } from '$lib/types';
 
 export function addToCollection(game: GameType) {
   collectionStore.add(game);
-  toast.send(`"${game.name}" added to collection!`, { duration: 3000, type: ToastType.INFO });
+  toast.send("Added to collection", { duration: 3000, type: ToastType.INFO });
 }
 
 export function removeFromCollection(game: GameType) {
   collectionStore.remove(game.id);
-  toast.send(`Removed "${game.name}" from collection!`, { duration: 3000, type: ToastType.INFO });
+  toast.send("Removed from collection", { duration: 3000, type: ToastType.INFO });
 }
