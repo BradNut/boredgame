fa26cbc91e5e4a7912e730b1edbd42726a633cde
Bradley Shellnut <bradleyshellnut@pm.me>
2022-10-12 23:49:06 -0500

    Adding server load to process games.

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,5 +1,14 @@
-import type { Actions } from '../$types';
+import type { Actions, PageServerLoad } from '../$types';
 import { Games } from '$root/search/actions';
+
+export const load: PageServerLoad = ({ request }) => {
+	console.log('page server load request', request)
+
+	return {
+    games: [],
+    totalCount: 0
+	};
+};
 
 export const actions: Actions = {
   default: Games.search,
