b23a101c0f14875994a4d03378e603c3df938948
Bradley Shellnut <bradleyshellnut@pm.me>
2023-06-22 16:52:25 -0700

    Modify data models, add slug and just kebab dependency, seed data for mechanics and categories if none exist in the DB.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client';
-// import userData from '../src/lib/data.json' assert { type: 'json' };
+import kebabCase from 'just-kebab-case';
+import categories from './categories.json' assert { type: 'json' };
+import mechanics from './mechanics.json' assert { type: 'json' };
 
 const prisma = new PrismaClient();
 
 async function main() {
 	console.log(`Start seeding ...`);
 
+	console.log('Creating roles ...');
 	const existingRoles = await prisma.role.findMany();
 	if (existingRoles.length === 0) {
 		await prisma.role.createMany({
@@ -14,6 +17,36 @@ async function main() {
 		console.log('Roles created.');
 	} else {
 		console.log('Roles already exist. No action taken.');
+	}
+
+	console.log('Creating Mechanics ...');
+	const existingMechanics = await prisma.mechanic.findMany();
+	if (existingMechanics.length === 0) {
+		for (const mechanic of mechanics.mechanics) {
+			await prisma.mechanic.create({
+				data: {
+					name: mechanic.name,
+					external_id: mechanic.id,
+					slug: kebabCase(mechanic.name)
+				}
+			});
+		}
+		console.log('Mechanics created.');
+	}
+
+	console.log('Creating Categories ...');
+	const existingCategories = await prisma.category.findMany();
+	if (existingCategories.length === 0) {
+		for (const category of categories.categories) {
+			await prisma.category.create({
+				data: {
+					name: category.name,
+					external_id: category.id,
+					slug: kebabCase(category.name)
+				}
+			});
+		}
+		console.log('Categories created.');
 	}
 
 	// for (const p of userData) {
