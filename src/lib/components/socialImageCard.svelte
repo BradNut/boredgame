9f6994650447720246fcf4b02310532beb09016a
Bradley Shellnut <bradleyshellnut@pm.me>
2023-09-13 21:06:22 -0700

    Reference image from static assets.

--- src/lib/components/socialImageCard.svelte
+++ src/lib/components/socialImageCard.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import * as Card from "$lib/components/ui/card";
-	import logo from '$lib/assets/bored-game.png';
 
 	export let title: string;
 </script>
 
 <Card.Root class="w-[350px]">
 	<Card.Header>
-		<Card.Title class="flex place-items-center gap-1"><div class="w-8 h-8"><img src={logo} alt="Bored Game" /></div>{title}</Card.Title>
+		<Card.Title class="flex place-items-center gap-1"><div class="w-8 h-8"><img src="/images/bored-game.png" alt="Bored Game" /></div>{title}</Card.Title>
 	</Card.Header>
 	<Card.Content>
 		<slot />
