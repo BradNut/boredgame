8e557498e3dc216b7aefa0c82f2956939273efd5
Bradley Shellnut <bradleyshellnut@pm.me>
2023-10-16 15:41:14 +1300

    Fix import of enum type from prisma by implementing a hacky workaroud in Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,29 @@ export default defineConfig({
 	plugins: [sveltekit(), rawFonts(['.ttf'])],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	css: {
+		devSourcemap: true,
+		preprocessorOptions: {
+			postcss: {
+				additionalData: `
+				@custom-media --below_small (width < 400px);
+				@custom-media --below_med (width < 700px);
+				@custom-media --below_large (width < 900px);
+				@custom-media --below_xlarge (width < 1200px);
+
+				@custom-media --above_small (width > 400px);
+				@custom-media --above_med (width > 700px);
+				@custom-media --above_large (width > 900px);
+				@custom-media --above_xlarge (width > 1200px);
+				`
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			'.prisma/client/index-browser': './node_modules/@prisma/client/index-browser.js'
+		}
 	}
 });
 
