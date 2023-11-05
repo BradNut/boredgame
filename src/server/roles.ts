a3d0c6757f88d2d8cbaf77661a8bc38e5302d680
Bradley Shellnut <bradleyshellnut@pm.me>
2023-11-04 17:11:58 -0700

    Fixing roles seed file.

--- src/server/roles.ts
+++ src/server/roles.ts
@@ -1,8 +1,8 @@
-import { prisma_client } from "../hooks.server";
+import prisma from "$lib/prisma";
 
 export async function add_user_to_role(user_id: string, role_name: string) {
 	// Find the role by its name
-	const role = await prisma_client.role.findUnique({
+	const role = await prisma.role.findUnique({
 		where: {
 			name: role_name
 		}
@@ -13,7 +13,7 @@ export async function add_user_to_role(user_id: string, role_name: string) {
 	}
 
 	// Create a UserRole entry linking the user and the role
-	const userRole = await prisma_client.userRole.create({
+	const userRole = await prisma.userRole.create({
 		data: {
 			user: {
 				connect: {
