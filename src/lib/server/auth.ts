3494876abbae659761f310f628390be884ff72e0
Bradley Shellnut <bradleyshellnut@protonmail.com>
2024-02-02 17:56:31 -0800

    Migrating to drizzle schemas.

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,10 +1,11 @@
 // lib/server/lucia.ts
 import { Lucia, TimeSpan } from 'lucia';
-import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import { DrizzleMySQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { dev } from '$app/environment';
-import prisma_client from '$lib/prisma';
+import db from '$lib/drizzle';
+import { sessionTable, userTable } from '../../schema';
 
-const adapter = new PrismaAdapter(prisma_client.session, prisma_client.user);
+const adapter = new DrizzleMySQLAdapter(db, sessionTable, userTable);
 
 export const lucia = new Lucia(adapter, {
 	getSessionAttributes: (attributes) => {
--- src/schema.ts
+++ src/schema.ts
@@ -1,27 +1,311 @@
-import { DrizzleMySQLAdapter } from "@lucia-auth/adapter-drizzle";
-import mysql from "mysql2/promise";
-import { int, mysqlEnum, mysqlTable, uniqueIndex, datetime, varchar, serial } from 'drizzle-orm/mysql-core';
-import { drizzle } from "drizzle-orm/mysql2";
+import { sql } from 'drizzle-orm';
+import { mysqlTable, datetime, varchar, boolean, timestamp } from 'drizzle-orm/mysql-core';
+import { nanoid } from 'nanoid';
 
-const connection = await mysql.createConnection();
-const db = drizzle(connection);
+model User {
+  id                String        @id @default(cuid())
+  username          String        @unique
+  hashed_password   String?
+  email             String?       @unique
+  firstName         String?
+  lastName          String?
+  roles             UserRole[]
+  verified          Boolean       @default(false)
+  receiveEmail      Boolean       @default(false)
+  collection        Collection?
+  wishlist          Wishlist?
+  list              List[]
+  theme             String        @default("system")
+  created_at        DateTime      @default(now()) @db.Timestamp(6)
+  updated_at        DateTime      @updatedAt @db.Timestamp(6)
+  sessions          Session[]
 
-const userTable = mysqlTable("user", {
+  @@map("users")
+}
+
+export const userTable = mysqlTable("users", {
 	id: varchar("id", {
 		length: 255
 	}).primaryKey()
+		.$defaultFn(() => nanoid()),
+	username: varchar("username", {
+		length: 255
+	}).unique(),
+	hashedPassword: varchar("hashed_password", {
+		length: 255
+	}),
+	email: varchar("email", {
+		length: 255
+	}).unique(),
+	firstName: varchar("first_name", {
+		length: 255
+	}),
+	lastName: varchar("last_name", {
+		length: 255
+	}),
+	verified: boolean("verified").default(false),
+	receiveEmail: boolean("receive_email").default(false),
+	theme: varchar("theme", {
+		length: 255
+	}).default("system"),
+	createdAt: timestamp("created_at").default(sql`(now(6))`),
+	updatedAt: timestamp("updated_at").default(sql`(now(6))`)
 });
 
-const sessionTable = mysqlTable("session", {
+// model Session {
+//   id             String     @id @unique
+//   userId         String
+//   ip_country     String
+//   ip_address     String
+//   expiresAt      DateTime
+//   user           User       @relation(references: [id], fields: [userId], onDelete: Cascade)
+
+//   @@index([userId])
+//   @@map("sessions")
+// }
+
+export const sessionTable = mysqlTable("sessions", {
 	id: varchar("id", {
 		length: 255
-	}).primaryKey(),
+	}).primaryKey()
+		.$defaultFn(() => nanoid()),
 	userId: varchar("user_id", {
 		length: 255
 	})
 		.notNull()
 		.references(() => userTable.id),
+	ipCountry: varchar("ip_country", {
+		length: 255
+	}),
+	ipAddress: varchar("ip_address", {
+		length: 255
+	}),
 	expiresAt: datetime("expires_at").notNull()
 });
 
-const adapter = new DrizzleMySQLAdapter(db, sessionTable, userTable);
+model Role {
+  id        String      @id @default(cuid())
+  name      String      @unique
+  userRoles UserRole[]
+
+  @@map("roles")
+}
+
+model UserRole {
+  id          String    @id @default(cuid())
+  user        User      @relation(fields: [user_id], references: [id])
+  user_id     String
+  role        Role      @relation(fields: [role_id], references: [id])
+  role_id     String
+  created_at  DateTime  @default(now()) @db.Timestamp(6)
+  updated_at  DateTime  @updatedAt  @db.Timestamp(6)
+
+  @@unique([user_id, role_id])
+  @@index([user_id])
+  @@index([role_id])
+  @@map("user_roles")
+}
+
+model Collection {
+  id                String               @id @default(cuid())
+  user_id           String          @unique
+  user              User               @relation(references: [id], fields: [user_id])
+  items CollectionItem[]
+
+  @@index([user_id])
+  @@map("collections")
+}
+
+model CollectionItem {
+  id String               @id @default(cuid())
+  collection_id String
+  collection Collection   @relation(references: [id], fields: [collection_id], onDelete: Cascade)
+  game_id String          @unique
+  game Game               @relation(references: [id], fields: [game_id])
+  times_played Int
+
+  @@index([game_id, collection_id])
+  @@index([game_id])
+  @@index([collection_id])
+  @@map("collection_items")
+}
+
+model Wishlist {
+  id String             @id @default(cuid())
+  user_id String        @unique
+  user User             @relation(references: [id], fields: [user_id])
+  items WishlistItem[]
+
+  @@index([user_id])
+  @@map("wishlists")
+}
+
+model WishlistItem {
+  id String                         @id @default(cuid())
+  wishlist_id String
+  wishlist Wishlist                 @relation(references: [id], fields: [wishlist_id], onDelete: Cascade)
+  game_id String                    @unique
+  game Game                         @relation(references: [id], fields: [game_id])
+  created_at DateTime               @default(now()) @db.Timestamp(6)
+  updated_at DateTime               @updatedAt @db.Timestamp(6)
+
+  @@index([game_id, wishlist_id])
+  @@index([game_id])
+  @@index([wishlist_id])
+  @@map("wishlist_items")
+}
+
+model List {
+  id String         @id @default(cuid())
+  name String
+  user_id String    @unique
+  user User         @relation(references: [id], fields: [user_id])
+  items ListItem[]
+
+  @@index([user_id])
+  @@map("lists")
+}
+
+model ListItem {
+  id String                     @id @default(cuid())
+  list_id String
+  list List                     @relation(references: [id], fields: [list_id], onDelete: Cascade)
+  game_id String                @unique
+  game Game                     @relation(references: [id], fields: [game_id])
+  created_at DateTime           @default(now()) @db.Timestamp(6)
+  updated_at DateTime           @updatedAt @db.Timestamp(6)
+
+  @@index([game_id, list_id])
+  @@index([game_id])
+  @@index([list_id])
+  @@map("list_items")
+}
+
+model Game {
+  id                      String              @id @default(cuid())
+  name                    String
+  slug                    String
+  description             String?             @db.LongText
+  year_published          Int?                @db.Year
+  min_players             Int?
+  max_players             Int?
+  playtime                Int?
+  min_playtime            Int?
+  max_playtime            Int?
+  min_age                 Int?
+  image_url               String?
+  thumb_url               String?
+  url                     String?
+  rules_url               String?
+  categories              Category[]
+  mechanics               Mechanic[]
+  designers               Designer[]
+  publishers              Publisher[]
+  artists                 Artist[]
+  names                   GameName[]
+  expansions              Expansion[]         @relation("BaseToExpansion")
+  expansion_of            Expansion[]         @relation("ExpansionToBase")
+  collection_items        CollectionItem[]
+  wishlist_items          WishlistItem[]
+  list_items              ListItem[]
+  external_id             Int                 @unique
+  last_sync_at            DateTime?           @db.Timestamp(6)
+  created_at              DateTime            @default(now()) @db.Timestamp(6)
+  updated_at              DateTime            @updatedAt @db.Timestamp(6)
+
+  @@fulltext([name])
+  @@fulltext([slug])
+  @@map("games")
+}
+
+model GameName {
+  id          String    @id @default(cuid())
+  name        String
+  slug        String
+  game_id     String
+  game        Game      @relation(references: [id], fields: [game_id])
+  created_at  DateTime  @default(now()) @db.Timestamp(6)
+  updated_at  DateTime  @updatedAt @db.Timestamp(6)
+
+  @@index([game_id])
+  @@map("game_names")
+}
+
+model Publisher {
+  id                  String    @id @default(cuid())
+  name                String
+  slug                String
+  external_id         Int       @unique
+  games               Game[]
+  created_at          DateTime  @default(now()) @db.Timestamp(6)
+  updated_at          DateTime  @updatedAt @db.Timestamp(6)
+
+  @@fulltext([name])
+  @@map("publishers")
+}
+
+model Category {
+  id            String    @id @default(cuid())
+  name          String
+  slug          String
+  games         Game[]
+  external_id   Int       @unique
+  created_at    DateTime  @default(now()) @db.Timestamp(6)
+  updated_at    DateTime  @updatedAt @db.Timestamp(6)
+
+  @@fulltext([name])
+  @@map("categories")
+}
+
+model Mechanic {
+  id            String    @id @default(cuid())
+  name          String
+  slug          String
+  games         Game[]
+  external_id   Int       @unique
+  created_at    DateTime  @default(now()) @db.Timestamp(6)
+  updated_at    DateTime  @updatedAt @db.Timestamp(6)
+
+  @@fulltext([name])
+  @@map("mechanics")
+}
+
+model Designer {
+  id                String    @id @default(cuid())
+  name              String
+  slug              String
+  external_id       Int       @unique
+  games             Game[]
+  created_at        DateTime  @default(now()) @db.Timestamp(6)
+  updated_at        DateTime  @updatedAt @db.Timestamp(6)
+
+  @@fulltext([name])
+  @@map("designers")
+}
+
+model Artist {
+  id            String    @id @default(cuid())
+  name          String
+  slug          String    @unique
+  external_id   Int       @unique
+  games         Game[]
+  created_at    DateTime  @default(now()) @db.Timestamp(6)
+  updated_at    DateTime  @updatedAt @db.Timestamp(6)
+
+  @@fulltext([name])
+  @@map("artists")
+}
+
+model Expansion {
+  id              String    @id @default(cuid())
+  base_game       Game      @relation(name: "BaseToExpansion", fields: [base_game_id], references: [id])
+  base_game_id    String
+  game            Game      @relation(name: "ExpansionToBase", fields: [game_id], references: [id])
+  game_id         String
+  created_at      DateTime  @default(now()) @db.Timestamp(6)
+  updated_at      DateTime  @updatedAt @db.Timestamp(6)
+
+  @@index([base_game_id])
+  @@index([game_id])
+  @@map("expansions")
+}
