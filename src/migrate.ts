679f59c0fd390970e13aab2f8c84e9a4ace28051
Bradley Shellnut <bradleyshellnut@pm.me>
2024-02-06 23:31:32 -0800

    Fixing env configs for migrate.

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,12 +1,17 @@
-import { drizzle } from "drizzle-orm/mysql2";
-import { migrate } from "drizzle-orm/mysql2/migrator";
-import { createConnection } from "mysql2";
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/mysql2';
+import { migrate } from 'drizzle-orm/mysql2/migrator';
+import { createConnection } from 'mysql2';
 
 const connection = createConnection({
-	uri: process.env.DATABASE_URL
-})
+	host: process.env.DATABASE_HOST,
+	port: 3306,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE_DB
+});
 const db = drizzle(connection);
 
-await migrate(db, { migrationsFolder: "drizzle" });
+await migrate(db, { migrationsFolder: 'drizzle' });
 
 await connection.end();
