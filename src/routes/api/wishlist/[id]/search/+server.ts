e7d1df356e38461c0fbde205ed43f0dea1588772
Bradley Shellnut <bradleyshellnut@pm.me>
2024-03-10 12:09:37 -0700

    Removing hopefully that last of prisma, convert the files to use drizzle, and fix shadcn-svelte pagination with newer lucide icon.

--- src/routes/api/wishlist/[id]/search/+server.ts
+++ src/routes/api/wishlist/[id]/search/+server.ts
@@ -1,18 +1,16 @@
 import { error, json } from '@sveltejs/kit';
-import prisma from '$lib/prisma';
-import { and, eq } from 'drizzle-orm';
-import { collection_items } from '../../../../../schema.js';
+import { eq } from 'drizzle-orm';
 import db from '$lib/drizzle.js';
+import { wishlist_items, wishlists } from '../../../../../schema.js';
 
 // Search a user's collection
 export async function GET({ url, locals, params }) {
 	const searchParams = Object.fromEntries(url.searchParams);
-	const q = searchParams?.q || '';
+	// const q = searchParams?.q || '';
 	const limit = parseInt(searchParams?.limit) || 10;
 	const skip = parseInt(searchParams?.skip) || 0;
 	const order = searchParams?.order || 'asc';
-	const sort = searchParams?.sort || 'name';
-	const collection_id = params.id;
+	const wishlist_id = params.id;
 	console.log('url', url);
 	console.log('username', locals?.user?.id);
 
@@ -22,32 +20,30 @@ export async function GET({ url, locals, params }) {
 		});
 	}
 
-	const collection = await db.collection.findUnique({
-		where: {
-			user_id: locals.user.id
-		}
+	const wishlist = await db.query.wishlists.findFirst({
+		where: eq(wishlists.user_id, locals?.user?.id)
 	});
-	console.log('collection', collection);
+	console.log('wishlist', wishlist);
 
-	if (!collection) {
-		console.log('Collection was not found');
-		error(404, { message: 'Collection was not found' });
+	if (!wishlist) {
+		console.log('Wishlist was not found');
+		error(404, { message: 'Wishlist was not found' });
 	}
 
 	try {
-		const itemsInCollection = await db.query.collection_items.findMany({
-			where: eq(collection_items.collection_id, collection_id),
+		const itemsInWishlist = await db.query.wishlist_items.findMany({
+			where: eq(wishlist_items.wishlist_id, wishlist_id),
 			with: {
 				game: {
 					columns: {
 						id: true,
 						name: true,
-						thumb_url: true,
-					},
+						thumb_url: true
+					}
 				}
 			},
-			orderBy: (collection_items, { asc, desc }) => {
-				const dbSort = sort === 'dateAdded' ? collection_items.created_at : collection_items.times_played;
+			orderBy: (wishlist_items, { asc, desc }) => {
+				const dbSort = wishlist_items.created_at;
 				if (order === 'asc') {
 					return asc(dbSort);
 				} else {
@@ -58,17 +54,7 @@ export async function GET({ url, locals, params }) {
 			limit
 		});
 
-		// include: {
-		// 		game: {
-		// 			select: {
-		// 				id: true,
-		// 				name: true,
-		// 				thumb_url: true
-		// 			}
-		// 		}
-		// 	},
-
-		return json(collection_items);
+		return json(itemsInWishlist);
 	} catch (e) {
 		console.error(e);
 		error(500, { message: 'Something went wrong' });
