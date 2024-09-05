af59ee1cfd2abc57dcdc1e753550a1220d1f2108
Bradley Shellnut <bradleyshellnut@pm.me>
2024-09-04 22:25:57 -0700

    Set back to build with vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
 import { preprocessMeltUI } from '@melt-ui/pp'
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import sequence from 'svelte-sequential-preprocessor'
 
