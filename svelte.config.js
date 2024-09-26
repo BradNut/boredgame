fc1a0731a62099a29f5cfa25dc00b314152271da
Bradley Shellnut <bradleyshellnut@protonmail.com>
2024-09-26 14:09:50 -0700

    Changing to adapter node

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
 import { preprocessMeltUI } from '@melt-ui/pp'
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import sequence from 'svelte-sequential-preprocessor'
 
