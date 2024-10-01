8cfb958832c20f1bd68f09b9347e30923cee8cdc
Bradley Shellnut <bradleyshellnut@protonmail.com>
2024-09-30 17:54:31 -0700

    Fix retrieve redis URL.

--- src/lib/server/api/providers/redis.provider.ts
+++ src/lib/server/api/providers/redis.provider.ts
@@ -5,7 +5,7 @@ import { config } from '../common/config'
 export const RedisProvider = Symbol('REDIS_TOKEN')
 export type RedisProvider = RedisClient
 container.register<RedisProvider>(RedisProvider, {
-	useValue: new RedisClient(config.REDIS_URL, {
+	useValue: new RedisClient(config.redis.url, {
 		maxRetriesPerRequest: null,
 	}),
 })
