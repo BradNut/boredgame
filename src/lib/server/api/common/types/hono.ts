9694e1784469343a24a21934accdbfaeed08af03
Bradley Shellnut <bradleyshellnut@protonmail.com>
2024-11-10 11:52:18 -0800

    Update deps and fix types

--- src/lib/server/api/common/types/hono.ts
+++ src/lib/server/api/common/types/hono.ts
@@ -1,8 +1,9 @@
-import type {Sessions} from '$lib/server/api/databases/postgres/tables';
-import type {Hono} from 'hono';
-import type {PinoLogger} from 'hono-pino';
-import type {Promisify, RateLimitInfo} from 'hono-rate-limiter';
-import type {User} from 'lucia';
+import type { Sessions } from '$lib/server/api/databases/postgres/tables';
+import type { Session } from '$lib/server/api/services/sessions.service';
+import type { Hono } from 'hono';
+import type { PinoLogger } from 'hono-pino';
+import type { Promisify, RateLimitInfo } from 'hono-rate-limiter';
+import type { User } from 'lucia';
 
 // export type AppOpenAPI = OpenAPIHono<AppBindings>;
 export type AppOpenAPI = Hono<AppBindings>;
@@ -10,7 +11,7 @@ export type AppOpenAPI = Hono<AppBindings>;
 export type AppBindings = {
 	Variables: {
 		logger: PinoLogger;
-		session: Sessions | null;
+		session: Session | null;
 		user: User | null;
 		rateLimit: RateLimitInfo;
 		rateLimitStore: {
@@ -23,7 +24,7 @@ export type AppBindings = {
 export type HonoTypes = {
 	Variables: {
 		logger: PinoLogger;
-		session: Sessions | null;
+		session: Session | null;
 		user: User | null;
 		rateLimit: RateLimitInfo;
 		rateLimitStore: {
